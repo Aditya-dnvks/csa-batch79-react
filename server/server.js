35edf1a6afd0baace638a6aa65f497a028e53ec0
Aditya Dasari <aditya.dnvks@gmail.com>
2025-01-30 10:08:28 +0530

    Server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: "./not_found" }); // Prevents error
 
 server.use(middlewares);
 server.use(router);
 
 const port = process.env.PORT || 3000;
-
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
